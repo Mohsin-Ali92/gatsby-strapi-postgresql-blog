8bd43849c541e64493d88da175d91698b740a27f
Mohsin-Ali92 <mohsin93790@gmail.com>
2021-05-05 19:47:56 +0530

    update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,9 +17,8 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.DEPLOY_URL
-          ? "https://gatsby-strapi-postgresql-blog.herokuapp.com"
-          : "http://localhost:1337",
+        apiURL: "https://gatsby-strapi-postgresql-blog.herokuapp.com",
+
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "articles",
